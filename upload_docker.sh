2fdc2d6ac73a17db5eccd251afa53d8addfb64a5
Dancanmilgo73 <dancanmilgo73@gmail.com>
2023-02-20 20:54:45 +0300

    Edit upload to docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="dancan/pricepredictionapp"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag pricepredictionapp $dockerpath:v1
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:v1
